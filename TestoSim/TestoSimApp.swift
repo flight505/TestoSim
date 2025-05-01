01c72c15e9f0d98cd9057517c1a12cb6957e2437
Example User <user@example.com>
2025-05-01 21:14:44 +0200

    Initial Commit

---
+++ TestoSim/TestoSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestoSimApp.swift
+//  TestoSim
+//
+//  Created by Jesper Vang on 01/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestoSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestoSimTests/TestoSimTests.swift
@@ -0,0 +1,16 @@
+//
+//  TestoSimTests.swift
+//  TestoSimTests
+//
+//  Created by Jesper Vang on 01/05/2025.
+//
+
+import Testing
+
+struct TestoSimTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
